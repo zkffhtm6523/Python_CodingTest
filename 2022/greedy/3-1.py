49c58355116d1c22b92a8783ba9441b7eeb4d088
zkffhtm6523 <zkffhtm6523@gmail.com>
2022-09-03 15:14:30 +0900

    거스름돈

--- 2022/greedy/3-1.py
+++ 2022/greedy/3-1.py
@@ -1,3 +1,14 @@
+# 거스름돈
 
-n = 10
-print(n)
+n = 1260
+count = 0
+
+# 큰 단위 부터 화폐 확인
+coin_types = [500, 100, 50, 10]
+
+for coin in coin_types:
+    count += n
+    n %= coin  # 500으로 나눈 나머지 값
+    print('n : ', n)
+
+print(count)
